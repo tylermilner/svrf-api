e6b4bda1f2b38a6f7b4befdfaa4c7b642cbd8e67
Roman <romanchaley@gmail.com>
2018-06-07 14:47:35 +0300

    Added java example

---
+++ examples/java/README.md
@@ -0,0 +1,1 @@
+# svrf-client-examples
---
+++ examples/java/src/main/java/example/App.java
@@ -0,0 +1,40 @@
+package example;
+
+import com.svrf.client.*;
+import com.svrf.client.model.*;
+import com.svrf.client.api.AuthenticateApi;
+import com.svrf.client.api.MediaApi;
+
+import java.io.File;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        AuthenticateApi apiInstance = new AuthenticateApi();
+        MediaApi mediaApi = new MediaApi();
+        
+        Body body = new Body();
+        body.apiKey("your api key");
+        
+        try {
+            AuthResponse result = apiInstance.appAuthenticatePost(body);
+            System.out.println(result);
+            ApiClient client = new ApiClient();
+            client.setApiKey(result.getToken());
+            mediaApi.setApiClient(client);
+
+            SingleMediaResponse m = mediaApi.vrIdGet("82989");
+            System.out.println(m);
+
+            TrendingResponse t = mediaApi.vrTrendingGet(5, null);
+            System.out.println(t);
+
+            SearchMediaResponse s = mediaApi.vrSearchqqGet("minsk", null, null, null, null);
+        } catch (ApiException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
