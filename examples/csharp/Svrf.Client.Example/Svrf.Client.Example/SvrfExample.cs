e1f31e805d73390ff116d2ccd0b06531fbc2b537
Roman <romanchaley@gmail.com>
2018-06-15 16:55:59 +0300

    Added csharp example

---
+++ examples/csharp/Svrf.Client.Example/Svrf.Client.Example/SvrfExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Svrf.Client.Api;
+using Svrf.Client.Model;
+
+namespace Svrf.Client.Example
+{
+    class SvrfExample
+    {
+        static void Main()
+        {
+            var authApi = new AuthenticateApi();
+            var mediaApi = new MediaApi();
+            var body = new Body("your key");
+
+            try
+            {
+                // Authenticate application
+                var authResult = authApi.AppAuthenticatePost(body);
+                Console.WriteLine(authResult.Token);
+                mediaApi.Configuration.ApiKey["x-app-token"] = authResult.Token;
+
+                var searchResult = mediaApi.VrSearchGet("minsk");
+                Console.WriteLine(searchResult.Media.FirstOrDefault()?.Title);
+
+                var trendingResult = mediaApi.VrTrendingGet();
+                Console.WriteLine(trendingResult.Media.FirstOrDefault()?.Title);
+
+                var idResult = mediaApi.VrIdGet("1337");
+                Console.WriteLine(idResult.Media?.Title);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
