84ac3ff252114c1d258b28b6b0057106834ebb64
AndreiEvstratenko <andrei.evstratenko@itechart-group.com>
2018-12-06 15:14:12 +0300

    Surf -> Svrf

--- examples/ARKitFaceFilterDemo/ARKitFaceFilterDemo/Classes/AppDelegate.swift
+++ examples/ARKitFaceFilterDemo/ARKitFaceFilterDemo/Classes/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // Authenticate with an API key. Do not forget add Surf API key into Info.Plist file.
+        // Authenticate with an API key. Do not forget add Svrf API key into Info.Plist file.
         SvrfSDK.authenticate(onSuccess: {
             
         }) { error in
